c06eb4ef36d9896ceffe4d718fc02266b6e75015
Crisandry Javier <crisandryj@gmail.com>
2024-07-21 15:31:57 -0400

    Feat: Add welcome to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Welcome from "./Greeting";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1>Test title</h1>
+      <Welcome></Welcome>
       <svg>
         <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
       </svg>
