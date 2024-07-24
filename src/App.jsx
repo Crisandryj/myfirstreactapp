37b406a81210c62ea257a6d57542fc6eb49acf35
Crisandry Javier <crisandryj@gmail.com>
2024-07-24 14:34:36 -0400

    Feat: Add person component with usestat

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Welcome from "./Greeting";
+import Person from "./Person";
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +13,7 @@ function App() {
     <>
       <h1>Test title</h1>
       <Welcome></Welcome>
+      <Person></Person>
       <svg>
         <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
       </svg>
